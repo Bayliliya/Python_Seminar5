930b72b9cd422fe755d55baabd10f9d07d2b5190
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-03-24 16:55:52 +0300

    Удалила нахождение через цикл

--- HW5/task_26.py
+++ HW5/task_26.py
@@ -2,14 +2,6 @@
 два числа A и B, и возводит число А в целую степень B с
 помощью рекурсии.
 '''
-# A = int(input("A: "))
-# B = int(input("B: "))
-# c = 1
-# i = 1
-# while i <= B:
-#     c = A*c
-#     i += 1
-# print(c)
 
 
 def rekurs_A_ext_B(A, B):
